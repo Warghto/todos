2e843e816b8a5b160725a1c88fc1c50d830ee40a
Avoid <116073567+Warghto@users.noreply.github.com>
2023-05-04 14:55:02 +0000

    Update TaskList.jsx and styles.css

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import AppContext from "../AppContext";
 
 function TaskList() {
-  const { stateTasks, setStateTasks, controlInputField, array, setArray } =
-    useContext(AppContext);
+  const { stateTasks, setStateTasks, array, setArray } = useContext(AppContext);
+  const [editIndex, setEditIndex] = useState(-1);
+  const [editText, setEditText] = useState("");
 
   const removeItem = (index) => {
     const newArray = [...array];
@@ -11,17 +12,44 @@ function TaskList() {
     setArray(newArray);
   };
 
+  const editItem = (index, value) => {
+    setEditIndex(index);
+    setEditText(value);
+  };
+
+  const saveItem = () => {
+    const editArray = [...array];
+    editArray[editIndex] = editText;
+    setArray(editArray);
+    setEditIndex(-1);
+    setEditText("");
+  };
+
   return (
     <div>
       <ul>
         {array.map((item, index) => (
           <li key={index}>
-            {item}
-            <button onClick={removeItem}>Delete</button>
+            {editIndex === index ? (
+              <input
+                type="text"
+                value={editText}
+                onChange={(event) => setEditText(event.target.value)}
+              />
+            ) : (
+              <div>{item}</div>
+            )}
+            <button onClick={() => editItem(index, item)}>Edit</button>
+            {editIndex === index ? (
+              <button onClick={saveItem}>Save</button>
+            ) : (
+              <button onClick={() => removeItem(index)}>Delete</button>
+            )}
           </li>
         ))}
       </ul>
     </div>
   );
 }
+
 export default TaskList;
